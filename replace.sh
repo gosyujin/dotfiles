3d7baa176a288812100b3a5ac87f5abd5dc257c6
kk_Ataka <kk_ataka@example.com>
2012-04-22 12:55:45 +0900

    簡易ログでツリーのSHA1値を見られるようにした

--- replace.sh
+++ replace.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LN=/bin/ln
-FROM=$HOME/github/dotfiles
+FROM=$HOME/works/github/dotfiles
 $LN -siv $FROM/.bashrc    $HOME
 $LN -siv $FROM/.gitconfig $HOME
 $LN -siv $FROM/.vimrc     $HOME
