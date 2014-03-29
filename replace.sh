6018b0851c86a5c1dbf6694c0de180b416738d8d
kk_Ataka <kk_ataka@ring.skr.jp>
2014-03-30 00:57:41 +0900

    確認無しの矯正上書き追加

--- replace.sh
+++ replace.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 FROM=$HOME/github/dotfiles
-/bin/ln -fsiv $FROM/.bashrc    $HOME
-/bin/ln -fsiv $FROM/.gitconfig $HOME
-/bin/ln -fsiv $FROM/.vimrc     $HOME
+OPTIONS=-fsv
+
+case $1 in
+  -n) echo no-interactive ;;
+  * ) OPTIONS=${OPTIONS}i ;;
+esac
+
+/bin/ln ${OPTIONS} $FROM/.bashrc    $HOME
+/bin/ln ${OPTIONS} $FROM/.gitconfig $HOME
+/bin/ln ${OPTIONS} $FROM/.vimrc     $HOME
